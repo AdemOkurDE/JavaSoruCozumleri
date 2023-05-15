153890013f27e21b06461a5ff187a9f7a7cbe3c4
AdemOkurDE <ademokur.tr@gmail.com>
2023-05-15 23:51:00 +0200

    2023Mayis15a

--- src/s05_methods/S04.java
+++ src/s05_methods/S04.java
@@ -14,20 +14,29 @@ public class S04 {
         int sayi= scanner.nextInt();
 
         System.out.println("Bolen sayisi : "+bolenSayisi(sayi));
+       /* int max=0,temp=0,bolen=0;
+        for (int i = 1; i <=sayi ; i++) {
 
+                    temp=bolenSayisi(i);
 
+                    if (temp>bolen){
+                        bolen=temp;
+                        max=i;
+                    }
+        }
+        System.out.println(max);
+        System.out.println("Bolen sayisi : "+bolenSayisi(max));
+*/
     }
     public static int bolenSayisi(int sayi){
         int bolenSayisi=0;
         for (int i = 1; i <=sayi ; i++) {
 
             if (sayi%i==0){
+                //System.out.print(i+" "); // bolenlerini yazdirmak istersek
                 bolenSayisi++;
             }
-
         }
-
-
 
         return bolenSayisi;
     }
